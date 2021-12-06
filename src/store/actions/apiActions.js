e79bda71cb819271cdc7cb9dcf5d394e6bf2418c
Gabriel Muelas <bimuelas@gmail.com>
2021-12-06 09:54:28 -0400

    add catalog in local json

--- src/store/actions/apiActions.js
+++ src/store/actions/apiActions.js
@@ -5,7 +5,8 @@ export const fetchCatalog = () => {
 	return (dispatch) => {
 		dispatch({ type: "LOAD_CATALOG_REQUEST" });
 		axios
-			.get("https://5e9935925eabe7001681c856.mockapi.io/api/v1/catalog")
+			//.get("https://5e9935925eabe7001681c856.mockapi.io/api/v1/catalog")
+			.get('catalog.json')
 			.then((res) => {
 				const products = catalogFilter(res.data);
 				dispatch({ type: "LOAD_CATALOG_SUCCESS", products });
